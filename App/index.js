13ccb754a8766df6193a9afd96de120af66237b1
cohl <claudio.heidel@gft.com>
2018-02-02 14:22:29 +0100

    update

--- App/index.js
+++ App/index.js
@@ -12,6 +12,7 @@ var serv_pfx     = 'microserv_socket_';
 var serv_timeout = 30; //seconds
 var serv_channel;
 var serv_subcriber;
+var socket_timeout = 30000; //seconds
 
 // Start a TCP Server
 const server = net.createServer(function(socket) {
@@ -27,6 +28,14 @@ const server = net.createServer(function(socket) {
  
     // Put this new client in the list
     clients.push(socket);
+    
+	socket.setTimeout(socket_timeout);
+	
+	socket.on('timeout', () => {
+		if (socket.username!== null) {
+			socket.sub.expire (socket.username, socket_timeout * 2);
+		}
+	});
 
     socket.sub.on("message", function(channel, message) {
         socket.write("sub channel " + channel + ": " + message + '\n');
@@ -48,7 +57,13 @@ const server = net.createServer(function(socket) {
                     if (d.length > 2) {
                         var channel = d[1].trim();
                         var msg = d[2];
-                        socket.pub.publish(channel, msg);
+                        socket.pub.publish(channel, msg, function(err, res) {
+                            if(err!==null) {
+                            	socket.write(err.toString() + "\n");
+                            } else {
+                            	socket.write(res.toString() + "\n");
+                            }
+                        });
                     }
                     break;
 
@@ -56,7 +71,13 @@ const server = net.createServer(function(socket) {
                     if (d.length > 1) {
                         if (socket.sub) {
                             var channel = d[1].trim();
-                            socket.sub.subscribe(channel);
+                            socket.sub.subscribe(channel, function(err, res) {
+								if(err!==null) {
+									socket.write(err.toString() + "\n");
+								} else {
+									socket.write(res.toString() + "\n");
+								}
+							});
                         }
                     }
                     break;
@@ -65,21 +86,36 @@ const server = net.createServer(function(socket) {
                     if (d.length > 1) {
                         if (socket.sub) {
                             var channel = d[1].trim();
-                            socket.sub.unsubscribe(channel);
+                            socket.sub.unsubscribe(channel, function(err, res) {
+								if(err!==null) {
+									socket.write(err.toString() + "\n");
+								} else {
+									socket.write(res.toString() + "\n");
+								}
+							});
                         }
                     }
                     break;
 
                 case 'login':
                     if (d.length > 1) {
+                    	if (socket.username != null) {
+                    		socket.sub.del(socket.username);
+                    		socket.username = null;
+                    	}
                         socket.username = d[1].trim();
+                        socket.sub.set(d[1].trim() , JSON.stringify ({"instance" : serv_channel }));
+						socket.sub.expire (d[1].trim(), socket_timeout * 2);
                         socket.write('Welcome ' + socket.username + "\n");
                     }
                     break;
 
                 case 'logout':
-                    socket.username = null;
-                    socket.write('Logout OK\n');
+                    if (socket.username != null) {
+                    	socket.sub.del(socket.username);
+                    	socket.username = null;
+                    	socket.write('Logout OK\n');
+                    }
                     break;
 
                 case 'exit':
@@ -91,11 +127,7 @@ const server = net.createServer(function(socket) {
                     if (d.length == 3) {
 						var username = d[1].trim();
 						var msgdata  = d[2];
-						if (msgto(msgdata , username, socket.username)) {
-							socket.write('OK\n');
-						} else {
-							socket.write('KO\n');
-						}
+						socket.write(msgto(msgdata , username, socket.username) + "\n");
                     } else {
                     	socket.write('bad syntax: msg <target_username> <text_message>\n');
                     }
@@ -140,6 +172,7 @@ const server = net.createServer(function(socket) {
 
     // Remove the client from the list when it leaves
     socket.on('end', function() {
+        if (socket.username != null) socket.sub.del(socket.username);
         socket.sub.unsubscribe();
         socket.sub.quit();
         socket.pub.quit();
@@ -151,43 +184,40 @@ const server = net.createServer(function(socket) {
   // handle errors here
    console.log('Socker server running at port', server.address().port);
    
-   serv_subcriber = redis.createClient(redis_port, redis_host);
-   
    //  Add Data for the Discovery Process
+   serv_subcriber = redis.createClient(redis_port, redis_host);
    serv_channel = serv_pfx + server.address().port;
-   serv_subcriber.set(serv_channel , JSON.stringify(server.address()), redis.print);
+   serv_subcriber.set(serv_channel , JSON.stringify(server.address()));
    serv_subcriber.expire (serv_channel, serv_timeout);
 
    // Add KeepAlive 
    setInterval(
    		function(){
 			var serv_subcriber = redis.createClient(redis_port, redis_host);
-   			serv_subcriber.expire (serv_channel, serv_timeout);
+			serv_subcriber.set(serv_channel , JSON.stringify(server.address()));
+			serv_subcriber.expire (serv_channel, serv_timeout);
    		}, 
    		(serv_timeout / 2) * 1000);
-   		
-   	
+
    	serv_subcriber.subscribe(serv_channel); 
    	serv_subcriber.on("message", function(channel, message) {
-   		if (channel == serv_channel) {
-            var d = csplit(message.toString('ascii'), ' ', 3);
-            if (d.length == 0) return;
-            switch (d[0].toLowerCase()) {
-                case 'msg':  // publish microserv_socket_49669 msg Pedro Claudio Hola amigo
-                    var from = d[1].trim();
-                    var to   = d[2];
-                    var msg  = d[3];
-                    msgto(msg , to, from);
-                    break;
-                    
-                case 'kill':
-                    kill(d[1]);
-                    break;
-                    
-                default:
-                    process.stdout.write('serv_subcriber->' + message);
-            }
-        }
+		var d = csplit(message.toString('ascii'), ' ', 3);
+		if (d.length == 0) return;
+		switch (d[0].toLowerCase()) {
+			case 'msg':  // publish microserv_socket_49669 msg Pedro Claudio Hola amigo
+				var from = d[1].trim();
+				var to   = d[2];
+				var msg  = d[3];
+				msgto(msg , to, from);
+				break;
+				
+			case 'kill':
+				kill(d[1]);
+				break;
+				
+			default:
+				process.stdout.write('serv_subcriber->' + message + "\n");
+		}
     });
    	
    	
@@ -197,17 +227,17 @@ const server = net.createServer(function(socket) {
 
     // Send to a specific username
     function msgto(message, to, from) {
-        var res = false;
+        var res = 0;
         clients.forEach(function(client) {
             // Don't want to send it to sender
             if (client.username == to) {
                 client.write("from " + from + ": " + message);
-                res = true;
+                res = 1;
                 return;
             }
         });
         
-        if(!res) {
+        if(res == 0) {
             // TODO
         	// This username isn't on the this instance, try to send in other instances.
         }
