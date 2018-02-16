169ae68dd3c3bc3dff59b673d8188284821c1956
cohl <claudio.heidel@gft.com>
2018-02-16 13:55:25 +0100

    update

--- App/test/dns.js
+++ App/test/dns.js
@@ -1,5 +1,59 @@
 #!/usr/bin/env node
 
-const dns = require('dns');
+//https://github.com/tjfontaine/node-dns
 
-console.log(dns.getServers());
+// https://www.infoq.com/articles/rest-discovery-dns
+
+var dns = require('native-dns');
+var server = dns.createServer();
+
+/*
+var records = {
+  "servicio.gft.corp" : [
+    {"address":"1.2.3.1","type":"A","ttl":60},
+    {"address":"1.2.3.2","type":"A","ttl":60},
+    {"address":"1.2.3.3","type":"A","ttl":60},
+    {"address":"1.2.3.4","type":"A","ttl":60},
+    {"address":"1.2.3.5","type":"A","ttl":60},
+  ]
+};
+
+console.log (records);
+*/
+
+server.on('request', function (request, response) {
+
+	console.log("\n\n\n\n\n\n\n");
+	console.log(request);
+
+	if (request.question[0].type == 33) {
+	  response.additional.push(dns.SRV({
+		name: request.question[0].name,
+		ttl: 600,
+		priority: 0,
+		weight: 0,
+		port: 5001,
+		target: request.question[0].name
+	  }));
+	} else {
+	  response.answer.push(dns.A({
+		name: request.question[0].name,
+		address: '127.0.0.1',
+		ttl: 600,
+	  }));
+	  response.answer.push(dns.A({
+		name: request.question[0].name,
+		address: '127.0.0.2',
+		ttl: 600,
+	  }));
+	}
+
+
+  response.send();
+});
+
+server.on('error', function (err, buff, req, res) {
+  console.log(err.stack);
+});
+
+server.serve(53);
