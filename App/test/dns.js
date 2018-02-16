7b7f72f80a7fe0a84f7afb4b73eb35e0b58d9602
cohl <claudio.heidel@gft.com>
2018-02-16 13:57:22 +0100

    update

--- App/test/dns.js
+++ App/test/dns.js
@@ -9,7 +9,7 @@ var server = dns.createServer();
 
 /*
 var records = {
-  "servicio.gft.corp" : [
+  "myservice.example.org" : [
     {"address":"1.2.3.1","type":"A","ttl":60},
     {"address":"1.2.3.2","type":"A","ttl":60},
     {"address":"1.2.3.3","type":"A","ttl":60},
@@ -17,8 +17,18 @@ var records = {
     {"address":"1.2.3.5","type":"A","ttl":60},
   ]
 };
-
 console.log (records);
+*/
+
+
+/*
+Testing
+
+run this script as sudo and open a second console to run the follow command:
+
+nslookup -type=SRV example.org  127.0.0.1
+ 
+ 
 */
 
 server.on('request', function (request, response) {
